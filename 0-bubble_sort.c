c3c49092e3c2e522c0a592c64351023d9963d92d
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-19 10:46:02 -0700

    betty task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 /**
- *add- Function that adds the top two elements of the stack.
- *@stack: stack struct
- *@line_number: line_number
+ *bubble_sort- Function that sorts an array of integers in ascending order
+ *using the Bubble sort algorithm
+ *@array: array to sort
+ *@size: size of array
  *Return: no return
  */
 
@@ -13,16 +14,19 @@ void bubble_sort(int *array, size_t size)
 
 	size_t i, j;
 	int dato;
-	
-	for (j = 0; j < size- 1; j++)
+
+	if (array == NULL)
+		return;
+
+	for (j = 0; j < size - 1; j++)
 	{
-		for(i = 0; i < size -j - 1; i++)
+		for (i = 0; i < size - j - 1; i++)
 		{
-			if (array[i] > array[i+1])
+			if (array[i] > array[i + 1])
 			{
 				dato = array[i];
-				array[i] = array[i+1];
-				array[i+1] = dato;
+				array[i] = array[i + 1];
+				array[i + 1] = dato;
 				print_array(array, size);
 			}
 		}
